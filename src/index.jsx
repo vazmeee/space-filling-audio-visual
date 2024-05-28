9fd02ab0927f8f113892472e68cfd6d5fcbc62f1
Srivamsi Sakirepalli <47343930+vazmeee@users.noreply.github.com>
2024-05-28 19:38:56 +0000

    fix

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css"; // Import the CSS file
-
-ReactDOM.render(<App />, document.getElementById("root"));
